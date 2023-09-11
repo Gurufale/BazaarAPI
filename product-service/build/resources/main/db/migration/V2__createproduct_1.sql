2cafc85de2fcd339ba79bb8d619646d791c4dc58
Gurufale <111334132+Gurufale@users.noreply.github.com>
2023-09-11 07:27:54 +0530

    Add files via upload

---
+++ product-service/build/resources/main/db/migration/V2__createproduct_1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE products(
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(50),
+    Description VARCHAR(50),
+    price float(53),
+    category varchar(50),
+    quantity int(10)
+);
